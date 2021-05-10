02d28998d17ab014e9bf80ab50b2a14f59a15092
bbaktaeho <lth9036@gmail.com>
2021-05-10 23:57:25 +0900

    Add: 2021-05-10

--- baekjoon/python/1120.py
+++ baekjoon/python/1120.py
@@ -1,15 +1,11 @@
 x, y = input().split()
+x_len = len(x)
 sub_len = len(y) - len(x)
-if sub_len == 0:
-    diff = 0
-    for i in range(len(x)):
-        if x[i] != y[i]: diff += 1
-    print(diff)
-else: 
-    diff = len(y)
-    for i in range(sub_len):
-        temp_diff = 0
-        for j in range(len(x)):
-            if x[j] != y[j+i]: temp_diff += 1 
-        diff = min(diff, temp_diff)
-    print(diff)
+
+diff = x_len
+for i in range(sub_len+1):
+    temp_diff = 0
+    for j in range(x_len):
+        if x[j] != y[i+j]: temp_diff += 1
+    diff = min(diff, temp_diff)
+print(diff)
